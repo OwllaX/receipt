4545d047ebc2da173da0c5eb58459a53ef66f990
OwllaX <axel72avs@outlook.com>
2020-11-25 10:14:42 -0600

    Testing the data with console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import test from "src/assets/test.json";
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,9 @@ export class AppComponent implements OnInit {
   url = 'https://northwind.now.sh/api/categories';
   receipt = [];
 
+  total : number = 0.00;
+  totalTax : number = 0.00;
+
   constructor(private http: HttpClient) { 
     this.http.get(this.url).toPromise().then(data => {
       for (let key in data) {
@@ -21,12 +25,38 @@ export class AppComponent implements OnInit {
     });
 
     console.log(this.receipt);
+
+    console.log(test);
+
+    this.getTotalTax();
    }
 
   ngOnInit() {      
-    setTimeout(() => {
+    /*setTimeout(() => {
       window.print();
       console.log('Ya se termino de cargar');
-    }, 4000);
+    }, 4000);*/
+  }
+
+  getTotal() {
+    
+  }
+
+  getTotalTax() {
+    let salesOrder = [];
+    let Tax : number = 0.00;
+    for (let key in test[0].SalesOrder) {
+      if(test[0].SalesOrder.hasOwnProperty(key))
+        salesOrder.push(test[0].SalesOrder)
+    }
+
+    /*for (let key in salesOrder) {
+      if (salesOrder.hasOwnProperty.call(key)) {
+        console.log(salesOrder[key]);
+        this.totalTax += (salesOrder[key].Price * salesOrder[key].Quantity) * salesOrder[key].Tax;
+      }
+
+      console.log(this.totalTax);
+    }*/
   }
 }
